33a9e46e5dbf15339a318bac13d5bbdc8c165f68
songmik <86704889+songmik@users.noreply.github.com>
2021-09-26 16:07:47 +0900

    0926

--- 17_2numberspopandplus.py
+++ 17_2numberspopandplus.py
@@ -1,9 +1,19 @@
 # 두 개 뽑아서 더하기 https://programmers.co.kr/learn/courses/30/lessons/68644
 
+# list와 set이용
+def solution(numbers):
+    answer=[]
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i]+numbers[j])
+        return sorted(list(set(answer))) # 집합 자료형은 중복을 허용하지 않기 때문에 리스트를 집합 자료형으로 바꾸게 되면 중복이 모두 없어짐
+
+
+# itertools import combination 이용
 from itertools import combinations
 def solution(numbers):
     answer=set()
-    for i in list(combinations(numbers,2)):
+    for i in list(combinations(numbers,2)): # numbers에 있는 숫자 중 2개를 뽑음
         answer.add(sum(i))
     return sorted(answer)
 
