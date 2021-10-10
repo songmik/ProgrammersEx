a28d1bcb82d8367dba8545e038898aa8db3e5636
songmik <86704889+songmik@users.noreply.github.com>
2021-10-10 17:36:02 +0900

    1010

---
+++ 26_lookingforkiminseoul.py
@@ -0,0 +1,7 @@
+# 서울에서 김서방 찾기 https://programmers.co.kr/learn/courses/30/lessons/12919
+
+def solution(seoul):
+    i=seoul.index('Kim')
+    return '김서방은 {0}에 있다'.format(i)
+
+# index 함수 : 리스트에 x의 값이 있으면 x의 위치 값을 돌려줌.
