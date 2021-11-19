c0ed928471d31a0aa1b09aea81027cfecb67cab1
Songmi <ksongmi0727@gmail.com>
2021-11-19 16:32:34 +0900

    1119

--- AlgorithmEx/13_palindromelinkedlist.py
+++ AlgorithmEx/13_palindromelinkedlist.py
@@ -1,4 +1,4 @@
-# 팰린드롬 연결 리스트
+# 13. 팰린드롬 연결 리스트(★)
 
 # ** 연결 리스트가 팰린드롬 구조인지 판별하라
 
@@ -61,23 +61,25 @@ def solution(self, head: ListNode) -> bool:
 
 """
 
+
 # 방법 3. 런너를 이용한 우아한 풀이
 def solution(self, head: ListNode) -> bool:
     rev = None
-    slow = fast = head
-    # 런너를 이용해 역순 연결 리스트 구성
+    slow = fast = head # head에서 시작
+
+    # 런너를 이용해 역순 연결 리스트 구성 -> next가 존재하지 않을 때까지 이동
     while fast and fast.next:
-        fast = fast.next.next
-        rev, rev.next, slow = slow, rev, slow.next
-    if fast:
-        slow - slow.next
+        fast = fast.next.next  # fast 런너는 두 칸씩 이동 , slow 한 칸씩 이동
+        rev, rev.next, slow = slow, rev, slow.next  # 역순 연결 리스트 : 현재 값을 slow로 교체하고 rev.next는 rev가 됨 => 앞에 계속 새로운 노드가 추가되는 형태
+    if fast: # 홀수와 짝수일 때 마지막 처리가 다르기 때문에, 홀수일 때 slow 런너가 한 칸 더 앞으로 이동하여 중앙의 값을 빗겨 나가야 함
+        slow - slow.next # fast가 None이 아니라는 경우로 간주할 수 있기 때문에 slow를 한 칸 더 이동해 마무리함
 
     # 팰린드롬 여부 확인
-    while rev and rev.val == slow.val:
+    while rev and rev.val == slow.val: 
         slow, rev = slow.next, rev.next
-    return not rev
+    return not rev # return not slow를 해도 똑같은 결과
 
 """
  런너 : 연결 리스트 순회 시 2개의 포인터를 동시에 사용하는 기법
- 
+  - 빠른 런너와 느린 런너를 각각 출발시키면, 빠른 런너가 끝에 다다를 때 느린 런너는 정확히 중간 지점에 도달하게 된다.
 """
