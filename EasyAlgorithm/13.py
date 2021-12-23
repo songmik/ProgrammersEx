3b2d3640331abfbdc9947050eaf1ae6e299a6646
Songmi <ksongmi0727@gmail.com>
2021-12-23 22:16:11 +0900

    1223

--- EasyAlgorithm/13.py
+++ EasyAlgorithm/13.py
@@ -19,3 +19,20 @@ def friends(g, start): # g : 친구 관계 그래프 , start : 모든 친구를 
             if x not in done: # 아직 큐에 추가된 적이 없는 사람을
                 q.append(x) # 큐에 추가
                 done.add(x) # 집합에 추가
+            
+
+# 친밀도를 계산
+def friends(g, start):
+    q=[] # 앞으로 처리해야 할 (사람이름, 친밀도) 튜플을 큐에 저장
+    done = set() # 이미 큐에 추가한 사람을 집합에 기록(중복 방지)
+    q.append((start,0))
+    done.add(start)
+
+    while q:
+        (p,d) = q.pop(0) # 큐에서 (사람이름, 친밀도) 정보를 p와 d로 꺼냄
+        print(p,d)
+        for x in g[p]: # 친구들 중에
+            if x not in done: # 아직 큐에 추가된 적이 없는 사람을
+                q.append((x,d+1)) # 친밀도를 1 증가시켜 큐에 추가하고
+                done.add(x) # 집합에도 추가
+
