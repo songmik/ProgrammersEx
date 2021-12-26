adf06c9a1f9fb84e1843f336645ad8c4d7aa7028
Songmi <ksongmi0727@gmail.com>
2021-12-27 00:50:52 +0900

    1227

--- Thisiscodingtest/03.Greedy.py
+++ Thisiscodingtest/03.Greedy.py
@@ -9,3 +9,21 @@ type = [500,100,50,10]
 for coin in type:
     count += n//coin
     n %= coin
+
+# 3-2 
+n, m, k = map(int, input().split()) # n, m, k를 공백으로 구분하여 입력받기
+data = list(map(int, input().split())) # n개의 수를 공백으로 구분하여 입력받기
+
+data.sort()
+first = data[n -1] # 가장 큰 수
+second = data[n-2] # 두 번째로 큰 수
+
+count = int(m/(k+1))* k # 가장 큰 수가 더해지는 횟수 계산
+count += m %(k+1)
+
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m-count) * second # 가장 작은 수 더하기
+
+print(result)
+
