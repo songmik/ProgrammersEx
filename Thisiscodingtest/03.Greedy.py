095066ec7472f45d35447d2bd804a69d0574d964
Songmi <ksongmi0727@gmail.com>
2021-12-27 20:45:39 +0900

    1227

--- Thisiscodingtest/03.Greedy.py
+++ Thisiscodingtest/03.Greedy.py
@@ -1,5 +1,6 @@
 # Chapter 03. 그리디
 
+
 # 3-1 . 거스름돈 
 n = 1260
 count = 0 # 초기화
@@ -9,6 +10,7 @@ type = [500,100,50,10]
 for coin in type:
     count += n//coin
     n %= coin
+
 
 # 3-2 
 n, m, k = map(int, input().split()) # n, m, k를 공백으로 구분하여 입력받기
@@ -27,3 +29,63 @@ result += (m-count) * second # 가장 작은 수 더하기
 
 print(result)
 
+
+# 3-3. minn() 함수를 이용하는 답안 예시
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data) # 현재 줄에서 '가장 작은 수' 찾기
+    result = max(result, min_value) # '가장 작은 수'들 중에서 가장 큰 수 찾기
+
+print(result)
+
+
+# 3-4. 2중 반복문 구조를 이용하는 답안 예시
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    result = max(result, min_value)
+
+print(result)
+
+
+# 3-5
+n, k = map(int, input().split())
+result = 0
+
+while n>= k:
+    while n % k != 0:
+        n -= 1
+        result += 1
+    n //= k
+    result += 1
+
+while n> 1:
+    n -=1
+    result += 1
+
+print(result)
+
+
+# 3-6
+n, k = map(int, input().split())
+result = 0
+
+while True:
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    if n <k:
+        break
+    result +=1
+    n //= k
+
+result +=(n-1)
+print(result)
